3b80c329b11a8bb8569303080e2def81aee2af7f
satyamprakash01 <144210958+satyamprakash01@users.noreply.github.com>
2023-09-08 14:31:06 +0530

    update

--- frontEnd/client/src/Components/homepage/homepage.js
+++ frontEnd/client/src/Components/homepage/homepage.js
@@ -9,6 +9,13 @@ const Homepage = (props) => {
 
     return (
         <div className="homepage">
+            //navbar(Home,applied internships,my profile,logout)
+            // company skills experience apply
+            // form to apply internship
+
+
+            
+
             <h1>Hello Homepage</h1>
             <div className="button" onClick={handleLogOut} >Logout</div>
         </div>
