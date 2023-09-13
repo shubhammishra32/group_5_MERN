620f815da4728fa795c7490084c81565e3efe404
shubhammishra32 <144117322+shubhammishra32@users.noreply.github.com>
2023-09-13 11:21:43 +0530

    changed API for get applied user data and change status

--- backEnd/controller/service.js
+++ backEnd/controller/service.js
@@ -12,7 +12,7 @@ export const mentorLogin = async (req, res) => {
       res.status(200).json({
         status: "success",
         data: { mentorData },
-        type: "mentor"
+        type: "mentor",
       });
     } else {
       res.status(400).json({
@@ -82,9 +82,13 @@ export const getinternship = async (req, res) => {
   }
 };
 
-export const getAllCandidateDetails = async (req, res) => {
+export const getCandidateDetails = async (req, res) => {
   try {
-    const usersData = await UserFormModel.find({}, { _id: 0, __v: 0 });
+    const { email, internshipId } = req.params;
+    const usersData = await UserFormModel.find(
+      { email, internshipId },
+      { _id: 0, __v: 0 }
+    );
 
     if (usersData.length > 0) {
       res.status(200).json({
@@ -107,27 +111,37 @@ export const getAllCandidateDetails = async (req, res) => {
 
 export const updateCandidateStatus = async (req, res) => {
   try {
-    const { email, status } = req.params;
+    const { internshipId } = req.params;
+    const { oldValue, newValue } = req.body;
+
+    const { status, email } = newValue;
 
     const userData = await UserFormModel.findOneAndUpdate(
-      { email },
+      { email, internshipId },
       { status },
       { new: true }
     );
+    // console.log(userData)
 
-    if (userData != null) {
+    const updateInternshipStatus = await Internship.findOneAndUpdate(
+      { _id: internshipId, ["applied"]: oldValue },
+      { $set: { ["applied"]: newValue } },
+      { new: true }
+    );
+    console.log("prindata", updateInternshipStatus);
+    if (userData) {
       res.status(200).json({
-        status: success,
+        status: "Success",
         data: { userData },
       });
     } else {
       res.status(400).json({
-        status: failed,
+        status: "Failed",
         data: { message: "User not found when update" },
       });
     }
   } catch (err) {
-    console.log("Something went wrong");
+    console.log(err);
     res.status(404).json({
       status: "Faild",
       message: err,
@@ -137,9 +151,10 @@ export const updateCandidateStatus = async (req, res) => {
 
 export const loginuser = async (req, res) => {
   try {
+ 
     const { email, password } = req.body;
-
-    const userData = await User.find({ email, password });
+    console.log("login", req.body)
+    const userData = await User.find();
 
     console.log(userData);
 
@@ -147,7 +162,7 @@ export const loginuser = async (req, res) => {
       res.status(200).json({
         status: "Login Successfull",
         data: { userData },
-        type: "candidate"
+        type: "candidate",
       });
     } else {
       res.status(400).json({
@@ -200,32 +215,31 @@ export const registeruser = async (req, res) => {
 export const userapplyform = async (req, res) => {
   try {
     const { internshipId, name, email, status } = req.body;
-   // console.log("insert data", req.body);
+    // console.log("insert data", req.body);
     const createData = await UserFormModel.create(req.body);
 
     const updateAppliedUser = await Internship.findOneAndUpdate(
-        {_id : internshipId},
-        { $push: { applied: {name, email, status} } },
-        { new: true }
-      );
-      console.log(updateAppliedUser);
-      console.log(createData);
+      { _id: internshipId },
+      { $push: { applied: { name, email, status } } },
+      { new: true }
+    );
+    console.log(updateAppliedUser);
+    console.log(createData);
 
-      if(updateAppliedUser && createData) {
-        console.log("success", updateAppliedUser)
-        res.status(200).json({
-            status: "success",
-            data: {
-              createData,
-            },
-          });
-      } else {
-        res.status(400).json({
-            status: "Faild",
-            message: "Invalid User"
-          });
-      }
-    
+    if (updateAppliedUser && createData) {
+      console.log("success", updateAppliedUser);
+      res.status(200).json({
+        status: "success",
+        data: {
+          createData,
+        },
+      });
+    } else {
+      res.status(400).json({
+        status: "Faild",
+        message: "Invalid User",
+      });
+    }
   } catch (err) {
     console.log(err);
     res.status(404).json({
--- backEnd/routes/route.js
+++ backEnd/routes/route.js
@@ -1,34 +1,35 @@
 import express from "express";
-import { getAllCandidateDetails, 
-        updateCandidateStatus, 
-        mentorLogin,
-        createinternship,
-        getinternship,
-        loginuser,
-        registeruser,
-        userapplyform} from "../controller/service.js";
-
+import {
+  updateCandidateStatus,
+  mentorLogin,
+  createinternship,
+  getinternship,
+  loginuser,
+  registeruser,
+  userapplyform,
+  getCandidateDetails,
+} from "../controller/service.js";
 
 const router = express.Router();
 
 // mentor routes
 
-router.post('/mentorlogin', mentorLogin); //done
+router.post("/mentorlogin", mentorLogin); //done
 
-router.post('/createinternship', createinternship) //done
+router.post("/createinternship", createinternship); //done
 
-router.get('/getinternship', getinternship) //done
+router.get("/getinternship", getinternship); //done
 
-router.get('/candidateDetails', getAllCandidateDetails);
+router.get("/candidateDetails/:email/:internshipId", getCandidateDetails);
 
-router.get('/scandidate/:email/:status', updateCandidateStatus);
+router.put("/scandidate/:internshipId", updateCandidateStatus);
 
 // user routes
 
-router.post('/login', loginuser); //done
+router.post("/login", loginuser); //done
 
-router.post('/register', registeruser); //done
+router.post("/register", registeruser); //done
 
-router.post('/applyform', userapplyform) //done
+router.post("/applyform", userapplyform); //done
 
 export default router;
